4e67301bfea6cffc5512fbaf81ef52b3c201cacf
rbrickmn <rbrickmn@gmail.com>
2025-01-02 16:07:59 -0500

    Add dynamic browser tab title functionality

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import React from "react";
 import Navbar from "../components/Navbar";
 
 const Projects = () => {
+  // Adjusts the tab title based on location
+  useEffect(() => {
+    document.title = "Riley Brickman | Projects"; // Set the tab title
+    return () => {
+      document.title = "Riley Brickman | Front-End Engineer"; // Reset the title when the component unmounts
+    };
+  }, []);
+
   return <Navbar />;
 };
 
